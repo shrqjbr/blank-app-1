fdacac57127f6aec4e2cdf6dacb68242a0bc56db
shrqjbr <shourouq11jbr@gmail.com>
2026-09-08 16:17:57 +0000

    Build StudyAI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🧠 StudyAI")
+
+st.write("Welcome to StudyAI!")
+
+name = st.text_input("What's your name?")
+
+if name:
+    st.write("Hello", name + "!")
